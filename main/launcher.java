e9ffc3ceb786b487c4916c9959c903d475f046bb
Nuvm <nuvmyt@gmail.com>
2019-03-22 17:03:32 -0400

    Ajouté des commentaires pour clarté. Première tentative de formattage pour permettre le testing du programme

---
+++ main/launcher.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Permet d'exécuter le programme de factures.
+ * @author Nuvm
+ *
+ */
+public class launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Factures();
+		System.exit(0);
+	}
+
+}
