01adf805495729727dcab092529e96d06ca5613c
Nuvm <nuvmyt@gmail.com>
2019-03-28 13:39:28 -0400

    Tests sur la première méthode ont été effectués

--- main/Factures.java
+++ main/Factures.java
@@ -16,83 +16,108 @@ public class Factures {
 	public String erreur = null;
 
 	public Factures() {
-		
+
 	}
-	
+
 	public Factures(boolean a) {
 		// Dialogue d'ouverture de fichier
 		FicLecture fic = new FicLecture();
 		lignes = fic.fileChoose();
 		lireFic();
-		if(erreur!=null)verifierErreurs();
-		if(erreur!=null)printFacture();
+		if (erreur != null)
+			verifierErreurs();
+		if (erreur != null)
+			printFacture();
+		if (erreur != null)
+			System.out.println("Une erreur est survenue: " + erreur);
 	}
 
 	public void setLignes(List<String> testLignes) {
 		lignes = new ArrayList<String>();
 		lignes = testLignes;
 	}
-	
+
 	/**
 	 * Lit et amasse les données des commandes.
 	 * 
 	 * @see FicLecture
 	 */
-	
+
 	public String lireFic() {
 		erreur = null;
-		
-
-		// Fin du while
-		boolean fin = false;
 
 		// l'index courant de l'itération des lignes du fichier
 		int compteur = 0;
 
 		// Le type, c'est-à-dire clients, plats ou commandes
 		int typeChose = -1;
-		if (lignes != null) {
-			while (compteur < lignes.size() && !fin) {
 
-				// La ligne à évaluer
-				String a = lignes.get(compteur);
+		// On vérifie que tous les types de données sont présents
+		
+		if (lignes == null)
+			
+			//Lignes est null
+			return erreur = "Erreur de lecture: Le fichier n'a pas pu être lu.";
+		
+		else if (!lignes.contains("Clients :")) 
 
-				if (a.indexOf(":") != -1) {
+			// Clients
+			return erreur = "Format non respecté: La donnée clients est indisponible.";
 
-					// Si il y a un :, on procède au prochain type d'objet (Ex.: "Menu:")
-					typeChose++;
+		else if (!lignes.contains("Plats :")) 
 
-				} else if (typeChose == -1) {
+			// Plats
+			return erreur = "Format non respecté: La donnée plats est indisponible.";
 
-					// Si le type est -1, la donnée client n'a jamais été présente
-					fin = true;
-					return "Format non respecté: La donnée client est indisponible.";
+		 else if (!lignes.contains("Commandes :")) 
 
-				} else if (a.indexOf("Fin") == -1) {
+			// Commandes
+			return erreur = "Format non respecté: La donnée commandes est indisponible.";
 
-					// Si on n'est pas à la dernière ligne (qui s'intitule "fin"), ajouter la ligne
-					// au type respectif
-					if (typeChose == 0)
-						clients.add(a);
+		 else if (!lignes.get(lignes.size() - 1).equals("Fin")) 
 
-					else if (typeChose == 1)
-						plats.add(a);
+			// La dernière ligne n'est pas fin
+			return erreur = "Format non respecté: La dernière ligne n'est pas 'fin'.";
 
-					else if (typeChose == 2)
-						commandes.add(a);
+		
+		while (compteur < lignes.size()) {
 
-					// Si le type est au-dessus de 2, il y a une anomalie
-					else
-						return "Format non respecté: 'Fin' n'est pas la dernière ligne.";
+			// La ligne à évaluer
+			String a = lignes.get(compteur);
 
-				}
+			if (a.indexOf(":") != -1) {
 
-				compteur++;
+				if (a.equals("Clients :"))
+					typeChose = 0;
+				else if (a.equals("Plats :"))
+					typeChose = 1;
+				else if (a.equals("Commandes :"))
+					typeChose = 2;
+				else
+					return erreur = "Format non respecté: Présence d'un mauvais ':'. Reçu: ' " + a + " '";
+
+			} else if (typeChose == -1) {
+
+				// Si le type est -1, la première ligne est une donnée
+				return erreur = "Format non respecté: La première ligne est une donnée. Reçu: ' " + a + " '";
+
+			} else if (a.indexOf("Fin") == -1) {
+
+				// Si on n'est pas à la dernière ligne (qui s'intitule "Fin"), ajouter la ligne
+				// au type respectif
+				if (typeChose == 0)
+					clients.add(a);
+
+				else if (typeChose == 1)
+					plats.add(a);
+
+				else if (typeChose == 2)
+					commandes.add(a);
 
 			}
 
-		} else {
-			return "Erreur de lecture: lignes est null.";
+			compteur++;
+
 		}
 		return erreur;
 
@@ -103,7 +128,7 @@ public class Factures {
 	 */
 
 	public String verifierErreurs() {
-		
+
 		// Vérification des erreurs dans la liste de commandes
 		for (int i = 0; i < commandes.size(); i++) {
 			// Commandes est formatté comme suit: <Client> <Plat>
@@ -124,10 +149,10 @@ public class Factures {
 				}
 				// Si le plat n'a pas été trouvé, retourner une erreur
 				if (!platFound)
-					erreur = "Le plat n'est pas dans le menu.";
+					return erreur = "Le plat n'est pas dans le menu.";
 			} else {
 				// Si le client n'a pas été trouvé, retourner une erreur
-				erreur = "Ce client n'est pas dans la liste.";
+				return erreur = "Ce client n'est pas dans la liste.";
 			}
 		}
 		return erreur;
@@ -139,43 +164,35 @@ public class Factures {
 	public void printFacture() {
 		// Print la facture
 
-		// Si il y a une erreur, afficher l'erreur
-		// TODO Deprecated. Il faut implémenter un moyen de retourner les erreurs durant
-		// le processus plutôt qu'à la dernière méthode du programme.
-		
-		if (erreur != "") {
-			System.out.println("ERREUR: " + erreur);
-		} else {
-			System.out.println("Bienvenue chez Barette!" + "\nFactures :");
-			// Pour chaque client
-			for (int i = 0; i < clients.size(); i++) {
-				// Le nom du client
-				String nom = clients.get(i);
-				// Total de la facture
-				double total = 0;
-				// Pour chaque commande
-				for (int j = 0; j < commandes.size(); j++) {
-					// On va cherche les 3 données, soit le nom du client, le plat, et la quantité
-					String[] comSplit = commandes.get(j).split(" ");
-					// On compare le nom du client au nom du client dans la commande
-					if (comSplit[0].equals(nom)) {
-						// Pour chaque plat
-						for (int k = 0; k < plats.size(); k++) {
-							// On obtient le nom du plat et le prix du plat
-							String[] platSplit = plats.get(k).split(" ");
-							// On compare le nom du plat au nom du plat dans la commande
-							if (platSplit[0].equals(comSplit[1])) {
-								// On rajoute au total l'équation qui est <quantité> * <prix> du plat
-								total += Double.parseDouble(comSplit[2]) * Double.parseDouble(platSplit[1]);
-							}
+		System.out.println("Bienvenue chez Barette!" + "\nFactures :");
+		// Pour chaque client
+		for (int i = 0; i < clients.size(); i++) {
+			// Le nom du client
+			String nom = clients.get(i);
+			// Total de la facture
+			double total = 0;
+			// Pour chaque commande
+			for (int j = 0; j < commandes.size(); j++) {
+				// On va cherche les 3 données, soit le nom du client, le plat, et la quantité
+				String[] comSplit = commandes.get(j).split(" ");
+				// On compare le nom du client au nom du client dans la commande
+				if (comSplit[0].equals(nom)) {
+					// Pour chaque plat
+					for (int k = 0; k < plats.size(); k++) {
+						// On obtient le nom du plat et le prix du plat
+						String[] platSplit = plats.get(k).split(" ");
+						// On compare le nom du plat au nom du plat dans la commande
+						if (platSplit[0].equals(comSplit[1])) {
+							// On rajoute au total l'équation qui est <quantité> * <prix> du plat
+							total += Double.parseDouble(comSplit[2]) * Double.parseDouble(platSplit[1]);
 						}
 					}
 				}
-				// Il n'y a pas de moyen d'échouer cette étape. L'échouement de cette étape
-				// signifie un problème dans la vérification des erreurs ou dans la recherche
-				// des commandes dans printFacture()
-				System.out.println(nom + " " + total + "$");
 			}
+			// Il n'y a pas de moyen d'échouer cette étape. L'échouement de cette étape
+			// signifie un problème dans la vérification des erreurs ou dans la recherche
+			// des commandes dans printFacture()
+			System.out.println(nom + " " + total + "$");
 		}
 	}
 }
--- test/FacturesTest.java
+++ test/FacturesTest.java
@@ -6,9 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import main.Factures;
@@ -24,11 +23,7 @@ public class FacturesTest {
 	
 	@After
 	public void afterTest() throws Exception {
-		facturesTest.lignes.clear();
-		facturesTest.clients.clear();
-		facturesTest.commandes.clear();
-		facturesTest.plats.clear();
-		facturesTest.erreur=null;
+		resetDonnees();
 	}
 	
 	@Test
@@ -37,8 +32,66 @@ public class FacturesTest {
 		//facturesTest.plats.addAll(Arrays.asList("Poutine 10.5","Frites 2.5","Repas_Poulet 15.75"));
 		//facturesTest.commandes.addAll(Arrays.asList("Roger Poutine 1","Céline Frites 2","Céline Repas_Poulet 1"));
 		//facturesTest.lignes.addAll(Arrays.asList("Clients :","Roger","Céline","Steeve","Plats :","Poutine 10.5","Frites 2.5","Repas_Poulet 15.75","Commandes :","Roger Poutine 1","Céline Frites 2","Céline Repas_Poulet 1","Fin"));
-		facturesTest.lignes.addAll(Arrays.asList("Clients :","Roger","Plats :","Poutine 10.5","Commandes :","Roger Poutine 1","Fin"));
+		
+		//Données valides
+		facturesTest.lignes = Arrays.asList("Clients :","Roger","Plats :","Poutine 10.5","Commandes :","Roger Poutine 1","Fin");
 		assertNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Manque la fin
+		facturesTest.lignes = Arrays.asList("Clients :","Roger","Plats :","Poutine 10.5","Commandes :","Roger Poutine 1");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Manque clients
+		facturesTest.lignes = Arrays.asList("Plats :","Poutine 10.5","Commandes :","Roger Poutine 1","Fin");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Manque plats
+		facturesTest.lignes = Arrays.asList("Clients :","Roger","Commandes :","Roger Poutine 1","Fin");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Manque commandes
+		facturesTest.lignes = Arrays.asList("Clients :","Roger","Plats :","Poutine 10.5","Fin");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Un ":" qui n'a pas lieu
+		facturesTest.lignes = Arrays.asList("Clients :","Roger","Plats :","Poutine 10.5","Commandes :","Roger: Poutine 1","Fin");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Lignes est vide
+		facturesTest.lignes = Arrays.asList("");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//La première ligne est une donnée
+		facturesTest.lignes = Arrays.asList("Joséphine","Roger","Plats :","Poutine 10.5","Commandes :","Roger Poutine 1","Fin");
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+		//Lignes est null
+		facturesTest.lignes = null;
+		assertNotNull(facturesTest.lireFic());
+		resetDonnees();
+		
+	}
+	
+	@Test
+	public void verifierErreursTest() {
+		assertNull(facturesTest.verifierErreurs());
+	}
+	
+	@Ignore
+	public void resetDonnees() {
+		facturesTest.lignes = new ArrayList<String>();
+		facturesTest.clients = new ArrayList<String>();
+		facturesTest.commandes = new ArrayList<String>();
+		facturesTest.plats = new ArrayList<String>();
+		facturesTest.erreur = null;
 	}
 
 }
